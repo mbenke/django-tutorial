72c7cf6df4f659ce79156e240e74f753a065cc38
Marcin Benke <marcin.benke@gmail.com>
2018-04-09 13:56:37 +0200

    animals: models, tests

--- mysite/animals/models.py
+++ mysite/animals/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=200)
+    sound = models.CharField(max_length=200)
+
+    class Meta:
+        app_label = 'animals'
+
+    def __str__(self):
+        return self.name
+
+    def speak(self):
+        return 'The {} says "{}"'.format(self.name, self.sound)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*'] # unsafe
 # Application definition
 
 INSTALLED_APPS = [
+    'animals.apps.AnimalsConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
