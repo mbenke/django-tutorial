3d2afb2411f64d3a437f1b573b69ff0cdb993cf6
Marcin Benke <marcin.benke@gmail.com>
2018-04-10 13:57:20 +0200

    AnimalFixtureTestCase

--- mysite/animals/tests.py
+++ mysite/animals/tests.py
@@ -2,10 +2,25 @@ from django.test import TestCase
 
 from .models import Animal
 
+
 class AnimalTestCase(TestCase):
     def setUp(self):
         Animal.objects.create(name="lion", sound="roar")
         Animal.objects.create(name="cat", sound="meow")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Animal.objects.get(name="lion")
+        cat = Animal.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
+
+
+class AnimalFixtureTestCase(TestCase):
+    fixtures = ['animals']
+
+    def setUp(self):
+        pass
 
     def test_animals_can_speak(self):
         """Animals that can speak are correctly identified"""
